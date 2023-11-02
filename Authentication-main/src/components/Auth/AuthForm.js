696d265721840289d50481a7235130c60343d7b5
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-11-02 14:03:09 +0000

    signUp page

--- Authentication-main/src/components/Auth/AuthForm.js
+++ Authentication-main/src/components/Auth/AuthForm.js
@@ -1,38 +1,84 @@
-import { useState, useRef } from 'react';
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  // nfn snippet
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+      // login things
+    } else {
+      // signUp things
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      );
+      console.log({ res });
+      if (res.ok) {
+        const data = await res.json();
+        // show an error modal
+        console.log(data);
+      }
+    }
+    setIsLoading(false);
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor="password">Your Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
             required
+            ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {isLoading ? (
+            <p>Sending Request ⏳</p>
+          ) : (
+            <button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? "Create new account" : "Login with existing account"}
+            </button>
+          )}
         </div>
       </form>
     </section>
