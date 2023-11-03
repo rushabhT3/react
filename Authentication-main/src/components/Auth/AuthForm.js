c4b281f426cf6d8cbf46b007c42531ab3a80a585
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-11-03 16:59:57 +0530

    login working m*7

--- Authentication-main/src/components/Auth/AuthForm.js
+++ Authentication-main/src/components/Auth/AuthForm.js
@@ -1,10 +1,16 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+// import { useHistory } from "react-router";
 
 import classes from "./AuthForm.module.css";
+// import { API_KEY } from "../../constants";
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
+  // const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authContext = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -13,22 +19,19 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  // nfn snippet
   const submitHandler = async (event) => {
     event.preventDefault();
-    setIsLoading(true);
 
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    let url;
-    if (isLogin) {
-      url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw";
-    } else {
-      url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw";
-    }
+    // optional: Add validation
+    setIsLoading(true);
+
+    const url = isLogin
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw"
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw";
+
     try {
       const res = await fetch(url, {
         method: "POST",
@@ -41,20 +44,25 @@ const AuthForm = () => {
           returnSecureToken: true,
         }),
       });
-      const data = await res.json();
+
       setIsLoading(false);
       if (res.ok) {
-        console.log(data);
-        return data;
+        const data = await res.json();
+        // console.log({ data });
+        authContext.login(data.idToken);
       } else {
-        let errorMessage = "Authentication Failed 😭";
-        throw new Error(errorMessage);
+        // const data = await res.json();
+        // if (data && data.error && data.error.message){
+        //   errorMsg = data.error.message
+        // }
+        const errorMsg = "Authentication failed 😭";
+        throw new Error(errorMsg);
       }
     } catch (error) {
       alert(error.message);
     }
   };
-  
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
@@ -73,18 +81,17 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
-          {isLoading ? (
-            <p>Sending Request ⏳</p>
-          ) : (
-            <button
-              type="button"
-              className={classes.toggle}
-              onClick={switchAuthModeHandler}
-            >
-              {isLogin ? "Create new account" : "Login with existing account"}
-            </button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
           )}
+          {isLoading && <p>Loading...</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
         </div>
       </form>
     </section>
--- Authentication-main/src/components/Profile/ProfileForm.js
+++ Authentication-main/src/components/Profile/ProfileForm.js
@@ -1,17 +1,49 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAmlkJVsdR4VRFHKasqsBt910ezfg-1Lcw",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+      }
+    );
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
